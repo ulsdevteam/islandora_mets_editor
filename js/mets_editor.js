f0c6c3626e6084c14f9c748dc32325b2f3f3392c
Brian Gillingham <bgilling@pitt.edu>
2017-04-17 15:11:22 +0000

    added code to determine which page image is currently clicked on - requires patch of xonomy library

--- js/mets_editor.js
+++ js/mets_editor.js
@@ -2,68 +2,105 @@
  * @file
  * Javascript file for islandora_mets_editor
  * 
- * The PHP code uses the constants for these 0, 1, 2:
- *       ISLANDORA_METS_EDITOR_SIMPLE = 0
- *       ISLANDORA_METS_EDITOR_BIG = 1
- *       ISLANDORA_METS_EDITOR_SMALL = 2
+ * The PHP code uses the constants for these 0, 1:
+ *       ISLANDORA_METS_EDITOR_XMLMODE = 0
+ *       ISLANDORA_METS_EDITOR_BASICMODE = 1
  * 
  */
 
 (function ($) {
 
-    // Add the event to start off the xonomy editor init routine.
-    Drupal.behaviors.yourBehaviorName = {
-      attach: function (context, settings) {
-        start_xonomy_editor();
-      }
-    };
+  // Add the event to start off the xonomy editor init routine.
+  Drupal.behaviors.yourBehaviorName = {
+    attach: function (context, settings) {
+      start_xonomy_editor();
+    }
+  };
 
-    $(document).ready(function() {
-        $('#edit-editor-layout-0').change(function() {
-            change_layout(0);
-        });
-        $('#edit-editor-layout-1').change(function() {
-            change_layout(1);
-        });
-        $('#edit-editor-layout-2').change(function() {
-            change_layout(2);
-        });
+  $(document).ready(function() {
+      $('#edit-editor-layout-0').change(function() {
+          change_layout(0);
+      });
+      $('#edit-editor-layout-1').change(function() {
+          change_layout(1);
+      });
 
-        var i = $('input[name=editor_layout]:checked', '#islandora-mets-editor-form').val();
-        change_layout(i);
-    });
+      var i = $('input[name=editor_layout]:checked', '#islandora-mets-editor-form').val();
+      change_layout(i);
+  });
   
-    function change_layout(i) {
-        alert('layout ' + i);
-        if (i < 1) {
-            $('.form-item-solr-query').show();
-            $('.form-item-list-of-pids').hide();
-            $('.form-item-collection').hide();
-            $('.form-item-model').hide();
-        }
-        if (i == 1) {
-            $('.form-item-solr-query').hide();
-            $('.form-item-list-of-pids').show();
-            $('.form-item-collection').hide();
-            $('.form-item-model').hide();
-        }
-        if (i == 2) {
-            $('.form-item-solr-query').hide();
-            $('.form-item-list-of-pids').hide();
-            $('.form-item-collection').show();
-            $('.form-item-model').hide();
-        }
-        if (i == 3) {
-            $('.form-item-solr-query').hide();
-            $('.form-item-list-of-pids').hide();
-            $('.form-item-collection').hide();
-            $('.form-item-model').show();
-        }
-    };
+  function change_layout(i) {
+    var mode='laic'
+    if (i < 1) {
+      mode='nerd';
+    }
+    Xonomy.setMode(mode);
+  };
   
 })(jQuery);
 
 function xonomy_click_passthrough(id, param) {
-    alert(id);
-    return Xonomy.click(id, param);
+  var node = $("#" + id);
+  var img_reference = findPageImageReference(node, "mets:file");
+
+  if (img_reference) {
+    $("#page_preview").html('<img id="theImg" src="/islandora/object/' + img_reference + '/datastream/JPG/view" />');
+  } else {
+    $("#page_preview").html();
+  }
+  
+  var node_div = node.children(' .children');
+  if (isNaN(node_div)) {
+    // Check parent element for the children children node...
+    node_div = node.closest('div')
+    if (isNaN(node_div)) {
+      // parent has no children
+    } else {
+      // no children, but parent had children
+      // node_div.html()
+    }
+  } else {
+    alert(node_div.html());
+  }
+
+  return Xonomy.click(id, param);
+}
+
+function findXrefValue(html) {
+  // <span data-name="xlink:href" data-value="pitt:31735051653552-0006" id="xonomy46" class="attribute">    
+  var xlinkHrefRegExp = /<span data-name=\"?xlink:href\" data-value=\"(.*?)\"\s/i;
+//   /meta\sproperty\=\"og\:title\"\scontent\=\"([A-Za-z0-9 _]*)\"/
+  var matches = [];
+  if (xlinkHrefRegExp.exec(html)) {
+    matches.push(RegExp.$1);
+  }
+  console.log(matches);
+  
+  return matches;
+}
+
+function findPageImageReference(start, finish){
+  start = start.parent();
+  var xhref = '';
+  if (start.attr('data-name') === finish){
+    xhref = findXrefValue(start.html());
+  }
+  start = start.parent();
+  if (start.attr('data-name') === finish){
+    xhref = findXrefValue(start.html());
+  }
+  start = start.parent();
+  if (start.attr('data-name') === finish){
+    xhref = findXrefValue(start.html());
+  }
+  start = start.parent();
+  if (start.attr('data-name') === finish){
+    xhref = findXrefValue(start.html());
+  }
+  start = start.parent();
+  if (start.attr('data-name') === finish){
+    xhref = findXrefValue(start.html());
+  }
+  console.log('xhref = ' + xhref);
+  return xhref;  
 }
